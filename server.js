9403b652253d36f5ebd4ae5fcb27dfc15785a4cb
Francisco Sandoval <frasandova@gmail.com>
2019-11-10 00:15:13 -0300

    first commit heroku

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Puerto Heroku
+
+const port = process.env.PORT || 3000;
+
 //mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',
 // mongoose.connection.openUri('mongodb+srv://syasoftadmin:jfcs4RWm5Bm3Bc4J@cluster0-0ak3v.mongodb.net/hospitalDB',
 mongoose.connection.openUri(process.env.URLDB,
@@ -32,7 +36,8 @@ app.use('/usuario', usuarioRoutes);
 app.use('/', appRoutes);
 
 // Escuchar Peticiones
-app.listen(3000,()=>{
-    // console.log('express Server puerto 3000 Online');
+app.listen(port,()=>{
+    //  console.log('express Server puerto 3000 Online');
+    console.log(`escuchando peticiones en el puerto ${ port } Online`);
     console.log('Node/Express: \x1b[36m%s\x1b[0m', 'online');
 });
