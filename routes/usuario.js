5c08ca17edd6465e1559ae05ca59cb42d99df2a5
Francisco Sandoval <frasandova@gmail.com>
2019-11-10 03:52:32 -0300

    config mailgun heroku

--- routes/usuario.js
+++ routes/usuario.js
@@ -10,8 +10,13 @@ var app = express();
 
 
 const mailgun = require("mailgun-js");
-const api_key = '63814692d26db4e39d58b751b7e1d713-f696beb4-a66bfe0a'
-const DOMAIN = 'sandboxe085727b425848c6aeb44f8c882e84df.mailgun.org';
+
+
+// const api_key = '63814692d26db4e39d58b751b7e1d713-f696beb4-a66bfe0a'
+// const DOMAIN = 'sandboxe085727b425848c6aeb44f8c882e84df.mailgun.org';
+
+const api_key = '299edaa55f50181f4986a8803164dd47-f696beb4-be15267a'
+const DOMAIN = 'sandboxbd6f70b3d54844db82e61ce69ebf91fc.mailgun.org';
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 const data = {
 	from: 'Francisco <frasandova@gmail.com>',
